d7164d2c61f4f1eb01a4b146dffa67ca4158cf80
Fumani09 <127149453+Fumani09@users.noreply.github.com>
2023-06-20 21:34:36 +0200

    Add files via upload

--- base_app.py
+++ base_app.py
@@ -35,6 +35,7 @@ tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl f
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
 
+
 # The main function where we will build the actual app
 def main():
 	"""Tweet Classifier App with Streamlit """
@@ -46,21 +47,80 @@ def main():
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
+	st.sidebar.subheader("Climate change tweet classification")
+	options = ["lr Prediction","Random Prediction","SVM Prediction","Naives Prediction", "Information"]
 	selection = st.sidebar.selectbox("Choose Option", options)
+
 
 	# Building out the "Information" page
 	if selection == "Information":
 		st.info("General Information")
 		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here")
+		st.markdown("""Some information here,
+        
+        1:hfch, 
+        2, 
+        0,
+        -1""")
 
 		st.subheader("Raw Twitter data and label")
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
 			st.write(raw[['sentiment', 'message']]) # will write the df to the page
 
+#Logistic Regression
 	# Building out the predication page
-	if selection == "Prediction":
+	if selection == "lr Prediction":
+		st.info("Prediction with ML Models")
+		# Creating a text box for user input
+		tweet_text = st.text_area("Enter Text","Type Here")
+
+		if st.button("Classify"):
+			# Transforming user input with vectorizer
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			# Load your .pkl file with the model of your choice + make predictions
+			# Try loading in multiple models to give the user a choice
+			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
+			lr_prediction = predictor.predict(vect_text)
+			st.success("Text Categorized as: {}".format(lr_prediction))
+			st.info("For Further Information on Categories: 1-neutral,2-believer, 0-non_believer,-1-man_made")
+
+#Random Forest
+	# Building out the predication page
+	if selection == "Random Prediction":
+		st.info("Prediction with ML Models")
+		# Creating a text box for user input
+		tweet_text = st.text_area("Enter Text","Type Here")
+
+		if st.button("Classify"):
+			# Transforming user input with vectorizer
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			# Load your .pkl file with the model of your choice + make predictions
+			# Try loading in multiple models to give the user a choice
+			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
+			prediction = predictor.predict(vect_text)
+			st.success("Text Categorized as: {}".format(lr_prediction))
+			st.info("For Further Information on Categories: 1-neutral,2-believer, 0-non_believer,-1-man_made")
+            
+#Support Vector Machine
+	# Building out the predication page
+	if selection == "SVM Prediction":
+		st.info("Prediction with ML Models")
+		# Creating a text box for user input
+		tweet_text = st.text_area("Enter Text","Type Here")
+
+		if st.button("Classify"):
+			# Transforming user input with vectorizer
+			vect_text = tweet_cv.transform([tweet_text]).toarray()
+			# Load your .pkl file with the model of your choice + make predictions
+			# Try loading in multiple models to give the user a choice
+			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
+			prediction = predictor.predict(vect_text)
+			st.success("Text Categorized as: {}".format(lr_prediction))
+			st.info("For Further Information on Categories: 1-neutral,2-believer, 0-non_believer,-1-man_made")
+            
+#Naives 
+	# Building out the predication page
+	if selection == "Naives Prediction":
 		st.info("Prediction with ML Models")
 		# Creating a text box for user input
 		tweet_text = st.text_area("Enter Text","Type Here")
@@ -77,6 +137,7 @@ def main():
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
 			st.success("Text Categorized as: {}".format(prediction))
+			st.info("For Further Information on Categories: 1-neutral,2-believer, 0-non_believer,-1-man_made")
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
